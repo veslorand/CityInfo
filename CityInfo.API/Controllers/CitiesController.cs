450db53586e33731774e5aa2f31e390d4667efa3
veslorand <veslorand@DESKTOP-Q7N5RK4>
2023-03-18 19:56:42 +0100

    Add project files.

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new { id = 1, Name = "New York"},
+                    new { id = 2, Name = "Antwerp"}
+                });
+        }
+
+
+    }
+}
