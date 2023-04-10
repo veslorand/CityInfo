c38aa41205d02104b71a2b439b169b7d64dc814d
veslorand <veslorand@gmail.com>
2023-04-10 19:36:40 +0200

    Try

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -25,6 +25,7 @@ builder.Services.AddControllers(options =>
     .AddNewtonsoftJson()
     .AddXmlDataContractSerializerFormatters();
 
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
