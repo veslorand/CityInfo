9378485a82d8323a99ba2cb9596761ae3e78be99
veslorand <veslorand@gmail.com>
2023-04-15 23:21:27 +0200

    Add CityInfoReporitory

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -41,6 +41,8 @@ builder.Services.AddSingleton<CitiesDataStore>();
 builder.Services.AddDbContext<CityInfoContext>(dbContextOptions => dbContextOptions.UseSqlite(
      builder.Configuration["ConnectionStrings:CityInfoDBConnectionString"])); // Azure KeyVault
 
+builder.Services.AddScoped<ICityInfoRepository, CityInfoRepository>();
+
 var app = builder.Build();
 
 //// Configure the HTTP request pipeline.
---
+++ CityInfo.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,12 @@
+﻿using CityInfo.API.Entities;
+
+namespace CityInfo.API.Services
+{
+    public interface ICityInfoRepository
+    {
+        Task<IEnumerable<City>> GetCitiesAsync();
+        Task<City?> GetCityAsync(int cityId, bool includePointOfInterest);
+        Task<PointOfInterest> GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId);
+        Task<IEnumerable<PointOfInterest?>> GetPointsOfInterestForCityAsync(int cityId);
+    }
+}
